b97ca1132cc121e457dbf034c9453b781260d2e7
José Sincal <3109171660407@ingenieria.usac.edu.gt>
2021-12-09 23:39:52 -0600

    add practice 1 - normal

---
+++ practica1/src/main/java/com/example/practica1/model/dataApi.java
@@ -0,0 +1,54 @@
+package com.example.practica1.model;
+
+public class dataApi {
+	private String name;
+	private String trackName;
+	private String type;
+	private String service;
+	private String url;
+	
+	public dataApi() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getTrackName() {
+		return trackName;
+	}
+	
+	public void setTrackName(String trackName) {
+		this.trackName = trackName;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getService() {
+		return service;
+	}
+	
+	public void setService(String service) {
+		this.service = service;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
---
+++ practica1/src/main/java/com/example/practica1/service/MisServicios.java
@@ -0,0 +1,99 @@
+package com.example.practica1.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.practica1.model.dataApi;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/rest")
+public class MisServicios {
+	static String urlApple = "https://itunes.apple.com/search?term=";
+	static String urlTV = "https://api.tvmaze.com/search/people?q=";
+	
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping(path="/test/{name}")
+	public String test(@PathVariable("name") String name) {
+		return "Hello " + name + ", Welcome to Practice 1";
+	}
+	
+	@GetMapping(path="/find/{name}")
+	public List<dataApi> find(@PathVariable("name") String name) throws JsonMappingException, JsonProcessingException {
+		List<dataApi> info = new ArrayList<dataApi>();
+		
+		info = getDataApple(urlApple + name);
+		info.addAll(getDataTV(urlTV + name));
+		
+		return info;
+	}
+	
+	private List<dataApi> getDataApple(String url) throws JsonMappingException, JsonProcessingException {
+		List<dataApi> info = new ArrayList<dataApi>();
+		dataApi data;
+		
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = mapper.readTree(response.getBody());
+		JsonNode results = root.path("results");
+		JsonNode artistName, trackName, type;
+		
+		for(JsonNode result :results) {
+			data  = new dataApi();
+			artistName = result.path("artistName");
+			trackName = result.path("trackName");
+			type = result.path("wrapperType");
+			
+			data.setName(artistName.toString().replace("\"", ""));
+			data.setTrackName(trackName.toString().replace("\"", ""));
+			data.setType(type.toString().replace("\"", ""));
+			data.setService("API iTunes");
+			data.setUrl(url);
+			
+			info.add(data);
+		}
+		
+		return info;
+	}
+	
+	private List<dataApi> getDataTV(String url) throws JsonMappingException, JsonProcessingException {
+		List<dataApi> info = new ArrayList<dataApi>();
+		dataApi data;
+
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = mapper.readTree(response.getBody());
+		JsonNode people, name;
+		
+		for(JsonNode result :root) {
+			data  = new dataApi();
+			
+			people = result.path("person");
+			name = people.path("name");
+			
+			data.setName(name.toString().replace("\"", ""));
+			data.setTrackName(null);
+			data.setType("People");
+			data.setService("API TVmaze");
+			data.setUrl(url);
+			
+			info.add(data);
+		}
+		
+		return info;
+	}
+}

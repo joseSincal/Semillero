b6aa88eaecaa14758e50ce8f138989b611e9d41f
Rudy Aarón Gopal Marroquín Garcia <3075624920403@ingenieria.usac.edu.gt>
2022-02-17 18:53:23 -0600

    Agregando nuevo query
    Se agrego el query para buscar una indermización mayor a un numero indicado en el servicio de Siniestros.

--- practica2/src/main/java/com/prac2/practica2/impl/SiniestroService.java
+++ practica2/src/main/java/com/prac2/practica2/impl/SiniestroService.java
@@ -1,5 +1,6 @@
 package com.prac2.practica2.impl;
 
+
 import java.util.List;
 import java.util.Optional;
 
@@ -19,40 +20,40 @@ public class SiniestroService implements SiniestroInterface {
 
 	@Autowired
 	SiniestroRepository siniestroRepository;
-	
+
 	@Autowired
 	PeritoRepository peritoRepository;
-	
+
 	@Override
 	public List<Siniestro> buscar() {
 		return siniestroRepository.findAll();
 	}
-	
+
 	@Override
 	public List<Siniestro> buscarAceptado(String estado) {
 		return siniestroRepository.findByAceptadoLike(estado);
 	}
-	
+
 	@Override
 	public List<Siniestro> buscarIndemnizacion(Double cantidad) {
 		return siniestroRepository.findByIndemnizacionLessThanEqual(cantidad);
 	}
-	
+
 	@Override
 	public List<Siniestro> buscarIndemnizacion(Double cantidad1, Double cantidad2) {
 		return siniestroRepository.findByIndemnizacionBetween(cantidad1, cantidad2);
 	}
-	
+
 	@Override
 	public List<Siniestro> buscarPoliza(Integer poliza) {
 		return siniestroRepository.findByNumeroPolizaOrderByIndemnizacionAsc(poliza);
 	}
-	
+
 	@Override
 	public List<Siniestro> buscarPoliza(String fecha, String estado) {
 		return siniestroRepository.findByFechaSiniestroLikeAndAceptadoEquals(fecha.replace("-", "/"), estado);
 	}
-	
+
 	@Override
 	public Siniestro guardar(SiniestroDto siniestroDto) {
 		Siniestro siniestro = convertirSiniestroDtoASiniestro(siniestroDto);
@@ -62,25 +63,30 @@ public class SiniestroService implements SiniestroInterface {
 	@Override
 	public void eliminar(int id) {
 		Optional<Siniestro> siniestro = siniestroRepository.findById(id);
-		if(siniestro.isPresent()) {
+		if (siniestro.isPresent()) {
 			siniestroRepository.delete(siniestro.get());
 		}
 	}
-	
+
 	@Override
 	public String agregarPerito(int id, int dni) {
 		Optional<Perito> perito = peritoRepository.findById(dni);
 		Optional<Siniestro> siniestro = siniestroRepository.findById(id);
-		
-		if(perito.isPresent() && siniestro.isPresent()) {
+
+		if (perito.isPresent() && siniestro.isPresent()) {
 			siniestro.get().setPerito(perito.get());
 			siniestroRepository.save(siniestro.get());
 			return "Perito agregado correctamente";
 		}
-		
+
 		return "No se pudo realizar la operación";
 	}
-	
+
+	@Override
+	public List<Siniestro> buscarIndermizacionDespues(Double indemnizacion) {
+		return siniestroRepository.findByIndemnizacionAfter(indemnizacion);
+	}
+
 	private Siniestro convertirSiniestroDtoASiniestro(SiniestroDto siniestroDto) {
 		ModelMapper mp = new ModelMapper();
 		return mp.map(siniestroDto, Siniestro.class);
--- practica2/src/main/java/com/prac2/practica2/ws/SiniestroInterface.java
+++ practica2/src/main/java/com/prac2/practica2/ws/SiniestroInterface.java
@@ -1,5 +1,6 @@
 package com.prac2.practica2.ws;
 
+
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,32 +20,35 @@ import com.prac2.practica2.entity.Siniestro;
 @RequestMapping(path = "/siniestro")
 @CrossOrigin
 public interface SiniestroInterface {
-	
+
 	@GetMapping(path = "/buscar")
 	public List<Siniestro> buscar();
-	
+
 	@GetMapping(path = "/buscar/por/aceptado/{estado}")
 	public List<Siniestro> buscarAceptado(@PathVariable String estado);
-	
+
 	@GetMapping(path = "/buscar/por/indemnizacion/{cantidad}")
 	public List<Siniestro> buscarIndemnizacion(@PathVariable Double cantidad);
-	
+
 	@GetMapping(path = "/buscar/por/indemnizacion/entre/{cantidad1}/{cantidad2}")
 	public List<Siniestro> buscarIndemnizacion(@PathVariable Double cantidad1, @PathVariable Double cantidad2);
-	
+
 	@GetMapping(path = "/buscar/por/poliza/{poliza}")
 	public List<Siniestro> buscarPoliza(@PathVariable Integer poliza);
-	
+
 	@GetMapping(path = "/buscar/por/fecha/{fecha}/aceptado/{estado}")
 	public List<Siniestro> buscarPoliza(@PathVariable String fecha, @PathVariable String estado);
-	
+
 	@PostMapping(path = "/guardar")
 	public Siniestro guardar(@RequestBody SiniestroDto siniestroDto);
-	
+
 	@DeleteMapping(path = "/eliminar/{id}")
 	public void eliminar(@PathVariable int id);
-	
+
 	@PutMapping(path = "/guardar/{id}/agregarPerito/{dni}")
 	public String agregarPerito(@PathVariable int id, @PathVariable int dni);
+
+	@GetMapping(value = "/buscar/por/indemnizacion/despues/{indemnizacion}")
+	public List<Siniestro> buscarIndermizacionDespues(@PathVariable Double indemnizacion);
 
 }
